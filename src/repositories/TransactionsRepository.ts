df675114315f8995bf54f78030c56fd18ca271cb
arglas <arglasdev@gmail.com>
2020-04-27 22:15:10 -0300

    implementacao desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transact => transact.type === 'income')
+      .reduce((total, transact) => {
+        return (total += transact.value);
+      }, 0);
+
+    const outcome = this.transactions
+      .filter(transact => transact.type === 'outcome')
+      .reduce((total, transact) => {
+        return (total += transact.value);
+      }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, type, value}: Request): Transaction {
+
+    if(type === 'outcome' ){
+
+      const {income, outcome, total} = this.transactionsRepository.getBalance();
+
+      if(value > total){
+        throw Error('The outcome value is not compatible with your balance.');
+      }
+    }
+
+    return this.transactionsRepository.create({title, type, value});
   }
 }
 
